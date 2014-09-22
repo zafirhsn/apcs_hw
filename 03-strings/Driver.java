0e6eb2e384e0c88c1f62d1a63b521247c65a22d7
Zafir Hasan <zafirhsn@gmail.com>
2014-09-21 22:00:01 -0400

    Added Driver.java

---
+++ 03-strings/Driver.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver {
+    public static void main (String[] args) {
+	String s = "zafir hasan";
+	
+	int x =  s.indexOf(" ");
+        String FirstName = s.substring(0,x);
+	String LastName = s.substring(x+1);
+
+	System.out.println(FirstName);
+	System.out.println(LastName);
+    }
+}
+
+	
