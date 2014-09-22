9850158cec825e38e812736c76682748be53ad95
Zafir <zafirhsn@gmail.com>
2014-09-22 18:58:31 -0400

    Added Driver.java (codingbat paractice)

---
+++ 04-StringStuff/Driver.java
@@ -0,0 +1,18 @@
+public class Driver {
+	
+	public String mixStart(String str) {
+		if (str.contains("ix")) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public String makeOutWord(String out, String word) {
+		return out.substring(0,2) + word + out.substring(2);
+	}	
+	
+	public String firstHalf(String str) {
+		return str.substring(0,(str.length()/2));
+	}
+}
