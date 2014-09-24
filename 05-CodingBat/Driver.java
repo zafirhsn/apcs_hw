a8a1c6809a283db2f54b680aa6d6bcd0a138facd
Zafir Hasan <zafirhsn@gmail.com>
2014-09-24 09:24:04 -0400

    Udpated Driver and StringStuff

--- 05-CodingBat/Driver.java
+++ 05-CodingBat/Driver.java
@@ -5,5 +5,8 @@ public class Driver {
 		System.out.println(Test.nonStart("Hello","There"));
 		System.out.println(Test.makeAbba("Hello","There"));
 		System.out.println(Test.diff21(45));
+		System.out.println(Test.nearHundred(95));
+		System.out.println(Test.teaParty(4,6));
+		System.out.println(Test.lastDigit(5,7,12));
 	}
 }
--- 05-CodingBat/StringStuff.java
+++ 05-CodingBat/StringStuff.java
@@ -1,20 +1,38 @@
 public class StringStuff {
 	public String nonStart(String a, String b) {
-		String x = a.substring(1);
-		String y = b.substring(1);
-		return x + y;
+	    String x = a.substring(1);
+	    String y = b.substring(1);
+	    return x + y;
 	}
 	
 	public String makeAbba(String a, String b) {
-		return a + b + b + a;
+	    return a + b + b + a;
 	}
 	
 	public int diff21(int n) {
-		if (n > 21) {
-			return (21 - n)*-1 * 2;
-		}
-		else {
-			return 21-n; 
-		}
+	    if (n > 21) {
+		return (21 - n)*-1 * 2;
+	    }
+	    return 21-n; 
 	}
+
+	public boolean nearHundred(int n) {
+	    return Math.abs(100-n) <= 10 || Math.abs(200-n) <= 10;
+	}
+
+        public int teaParty(int tea, int candy) {
+	    if (tea < 5 || candy < 5) {
+		return 0;
+	    }
+	    if (tea >= 2 * candy || candy >= 2 * tea) {
+		return 2;
+	    }
+	    return 1;
+	}
+
+public boolean lastDigit(int a, int b, int c) {
+  return (a % 10 == c % 10 || a % 10 == b % 10 || c % 10 == b % 10);
+ 
+}
+
 }
