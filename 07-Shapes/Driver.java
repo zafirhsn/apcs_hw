4e07d9bd50e29d8c3cd97ce207075544067fc1c5
Zafir Hasan <zafirhsn@gmail.com>
2014-10-04 17:24:10 -0400

    Updated Shapes.java to include diamond and tri4

--- 07-Shapes/Driver.java
+++ 07-Shapes/Driver.java
@@ -6,8 +6,24 @@ public class Driver {
 	System.out.println(s.box(4,5));
 	
 	System.out.println(s.tri1(4));
+	System.out.println(s.tri1(6));
+	System.out.println(s.tri1(2));
+
 	System.out.println(s.tri2(4));
+	System.out.println(s.tri2(6));
+	System.out.println(s.tri2(2));
+
 	System.out.println(s.tri3(4));
+	System.out.println(s.tri3(6));
+	System.out.println(s.tri3(2));
+
 	System.out.println(s.diamond(5));
+	System.out.println(s.diamond(7));
+	System.out.println(s.diamond(3));
+
+	System.out.println(s.tri4(4));
+	System.out.println(s.tri4(6));
+	System.out.println(s.tri4(2));
+
     }
 }
--- 07-Shapes/Shapes.java
+++ 07-Shapes/Shapes.java
@@ -2,12 +2,11 @@
 public class Shapes {
     public String box(int r, int c) {
 	String s = "";
-
-	while (r>0){
-	    int cc = 0;
-	    while (cc < c){
+	while (r > 0){
+	    int counter = 0;
+	    while (counter < c){
 		s += "*";
-		cc += 1;
+		counter += 1;
 	    }
 	    s = s + "\n";
 	    r--;
@@ -16,124 +15,105 @@ public class Shapes {
 	return s;
     }
 	
-	//Took about 10 minutes
-	public String tri1(int h) {
-		String ans = "";
-		String cc = "";
-		while (h > 0) {
-			cc += "*";
-			ans += cc + "\n";
-			h -= 1;
-		}
-		return ans;
+    //Took about 10 minutes
+    public String tri1(int h) {
+	String s = "";
+	String cc = "";
+	while (h > 0) {
+	    cc += "*";
+	    s += cc + "\n";
+	    h -= 1;
 	}
+	return s;
+    }
 	
-	//Took about 25 minutes
-	public String tri2(int h) {
-		String ans = "";
-		String cc = "";
-		int x = 1;
-		int z = 1;
-		int y = 1;
-		while (h > 0) {
-			while (x < h) {
-				cc += " ";
-				x += 1;
-			}
-			while (y > 0) {
-				cc += "*";
-				y -= 1;
-			}
-			ans += cc + "\n";
-			h -= 1;
-			x = 1;
-			z += 1;
-			y = z;
-			cc = "";
-		}
-		return ans;
+    //Took about 25 minutes
+    public String tri2(int h) {
+        String s = "";
+	int space = h-1;
+	int star = 1;
+	int r = h;
+	while (r > 0) {
+	    while (space > 0) { 
+		s += " ";
+		space -= 1;
+	    }
+	    while (star > 0) {
+		s += "*";
+		star -= 1;
+	    }
+	    r -= 1;
+	    space = r-1;
+	    star = h-space;
+	    s += "\n";
 	}
+	return s;
+
+    }
 	
 	
-	//Took about 20 minutes
-	public String tri3 (int h) {
-		int space = h-1;
-		int star = 1;
-		int z = 1;
-		int x = 1;
-		int row = h;
-		String finalString = "";
-		String cc = "";
-		while (row > 0) {
-			while (space > 0) {
-				cc += " ";
-				space -= 1;
-			}
-			z++;
-			space = h - z;
-			while (star > 0) {
-				cc += "*";
-				star -= 1;
-			}
-			x += 2;
-			star = x;
-			finalString += cc + "\n";
-			cc = "";
-			row -= 1;
-		}
-		return finalString;
+    //Took about 20 minutes
+    public String tri3 (int h) {
+	String s = ""; 
+	int r = h;
+	int space = r-1;
+	int star = 2*h-space-r; 
+	while (r > 0) {
+	    while (space > 0) {
+		s += " ";
+		space -= 1;
+	    }
+	    while (star > 0) {
+		s += "*";
+		star -= 1;
+	    }
+	    r -= 1;
+	    space = r-1;
+	    star = 2*h-space-r;
+	    s += "\n";
 	}
+	return s;
+
+    }
 	
-	//Could not get this
-	public String diamond(int h) {
-		Shapes test1 = new Shapes();
-		
-		String ans = test1.tri3(h);
-		String cc = "";
-		
-		int y = h;
-		int z = h;
-		int x = 1;
-		int w = x;
-	while (h > 0) {	
-		while (z > 0) {
-			cc += "*";
-			z -= 1;
-		}
-		ans += cc + "\n";
-		y -= 2;
-		z = y;
-		
-		
-		while (x > 0) {
-			cc += " ";
-			x -= 1;
-		}
-		w += 1;
-		x = w;
-		while (z > 0) {
-			cc += "*";
-			z -= 1;
-		}
-		y -= 2;
-		z = y;
-		ans += cc + "\n";
-		h -= 1;
-		
+    
+    public String diamond(int h) {
+	String s = tri3((h/2)+1);
+	int r = h-2;
+	int space = h-r-((h-r)/2);
+	int star = r;
+	while (r > 0) {
+	    while (space > 0) {
+		s += " ";
+		space -= 1;
+	    }
+	    while (star > 0) {
+		s += "*";
+		star -= 1;
+	    }
+	    r -= 2;
+	    space = h-r-((h-r)/2);
+	    star = r;
+	    s += "\n";
 	}
-	return ans;
-	
-	
-}
-		
-		
-		
-		
-		
-		
-	
-	
-	
-	
-	
+	return s;	
+    }
+
+
+    public String tri4(int h) {
+	String s = "";
+	int r;
+	int space;
+	int star;
+	for (r=h;r>0;r-=1) {
+	    for (space=h-r;space>0;space-=1) {
+		s += " ";
+	    }
+	    for (star=r;star>0;star-=1) {
+		s += "*";
+	    }
+	    s += "\n";
+	}
+	return s;
+    }
 }
