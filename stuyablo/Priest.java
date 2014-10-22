0f0174f808767344d2ee203d864517072c68f636
Zafir Hasan <zafirhsn@gmail.com>
2014-10-22 09:05:13 -0400

    Added game

---
+++ stuyablo/Priest.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+import java.util.*;
+import java.io.*;
+
+public class Priest extends Basechar {
+    //---name---
+    //MUST try to switch these into void methods
+    public Priest(String n){
+        super(n);
+    }
+    public String getName(){
+        return super.getName();
+    }
+    //---health---
+    
+    public void getHealth(){
+        super.getHealth();
+    }
+}
