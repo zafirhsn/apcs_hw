6c111bd2c10002b8cdb1aaf033dbdf392dc464cf
Zafir <zafirhsn@gmail.com>
2014-10-23 18:11:41 -0400

    Finished HW 13

---
+++ 13-startingArrays/Driver.java
@@ -0,0 +1,13 @@
+
+public class Driver {
+    public static void  main(String[] args){
+	Shapes s =  new Shapes();
+	System.out.println(s.box(5,4));
+	System.out.println(s.box(4,5));
+	
+	System.out.println(s.tri1(4));
+	System.out.println(s.tri2(4));
+	System.out.println(s.tri3(4));
+	System.out.println(s.diamond(5));
+    }
+}
